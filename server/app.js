1ad56e719ef2ec39f2b5b7ebcf554df8be87586b
xILxELECTRO <xilxelectro1@gmail.com>
2026-09-19 23:27:44 +0000

    ✨Feat: Carpeta back-end Set up Express application with routung an error

--- server/app.js
+++ server/app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //Configurar la carpeta publica para servir archivos estaticos
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..','public')));
 
 //Registrar las rutas de la aplicacion
 app.use('/', indexRouter);
