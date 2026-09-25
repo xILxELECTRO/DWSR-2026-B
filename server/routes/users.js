ccca0befb6dc6c5cd95fa1835e3592a9f6632703
xILxELECTRO <xilxelectro1@gmail.com>
2026-09-25 19:01:05 +0000

    ✨ Feat: Nodemon Update users route response and enhance package.json for nodemon

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('<h1> style:"red"> LISTA DE AMIGOS Y EXES<h1>');
 });
 
 export default router;
